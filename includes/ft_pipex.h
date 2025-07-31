dc3e82708b0a6c9eceb64d5b30d9c03496970b05
Ertugrul Lenovo Ubuntu Terminal <ertu.pakdamar@gmail.com>
2025-07-31 15:13:58 +0300

    refactor: update header file and remove unused include

--- includes/ft_pipex.h
+++ includes/ft_pipex.h
@@ -6,7 +6,7 @@
 /*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 09:51:48 by epakdama          #+#    #+#             */
-/*   Updated: 2025/07/31 13:02:44 by epakdama         ###   ########.fr       */
+/*   Updated: 2025/07/31 15:12:38 by epakdama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 # include "get_next_line_bonus.h"
 # include "libft.h"
 # include <fcntl.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <sys/wait.h>
 # include <unistd.h>
